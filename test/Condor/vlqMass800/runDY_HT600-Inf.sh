d131ce1b6b530a30aa6d3b3c490f22a7ab86b9e9
tmitchell <tyler106@ksu.edu>
2016-04-28 09:20:01 -0500

    add condor files

---
+++ test/Condor/vlqMass800/runDY_HT600-Inf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src
+eval `scram runtime -sh`
+
+cd ${_CONDOR_SCRATCH_DIR}
+echo ${_CONDOR_SCRATCH_DIR}
+let "sample=${1}+1"
+cp /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src/Analysis/VLQAna/test/Condor/vlqMass800/dy_HT600-Inf/dy_HT600-Inf_${sample}.py .
+cp /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src/Analysis/VLQAna/test/*.txt .
+cp /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src/Analysis/VLQAna/test/*.root .
+cp /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src/Analysis/VLQAna/test/inputFiles_cfi.py .
+cmsRun dy_HT600-Inf_${sample}.py
+rm PU*
+rm Run*
+rm dataset*
+rm hnpv*
+rm os2lana*
+xrdcp *.root root://cmseos.fnal.gov//store/user/tmitchel/condor/dy_HT600-Inf
+rm dy_HT600-Inf_${sample}.py
+rm *.root
+ls
+echo "DONE!"
---
+++ test/Condor/vlqMass800/run_bprime1000.sh~
@@ -0,0 +1,25 @@
+#!/bin/bash                                                                                                                                 \
+
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src
+eval `scram runtime -sh`
+
+cd ${_CONDOR_SCRATCH_DIR}
+echo ${_CONDOR_SCRATCH_DIR}
+let "sample=${1}+1"
+cp /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src/Analysis/VLQAna/test/Condor/vlqMass1200/bprime1000/bprime1000_${sample}.py .
+cp /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src/Analysis/VLQAna/test/*.txt .
+cp /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src/Analysis/VLQAna/test/*.root .
+cp /uscms_data/d3/tmitchel/Analysis2016/CMSSW_7_4_16_patch2/src/Analysis/VLQAna/test/inputFiles_cfi.py .
+cmsRun bprime1000_${sample}.py
+rm PU*
+rm Run*
+rm dataset*
+rm hnpv*
+rm os2lana*
+xrdcp *.root root://cmseos.fnal.gov//store/user/tmitchel/condor/bprime1000
+rm bpime1000_${sample}.py
+rm *.root
+ls
+echo "DONE!"
