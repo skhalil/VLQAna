d54b4ddf76760cadbc992dd9c21e7d87001a0e6a
tmitchell <tyler106@ksu.edu>
2016-05-02 11:49:42 -0500

    fix MET

--- interface/METMaker.h
+++ interface/METMaker.h
@@ -4,13 +4,14 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "AnalysisDataFormats/BoostedObjects/interface/Met.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
 
 using namespace std;
 using namespace edm;
 
 class METMaker {
 public:
-	METMaker (edm::ParameterSet const& pars) ;
+        METMaker (edm::ParameterSet const& pars, edm::ConsumesCollector && iC) ;
 	~METMaker () ;
 	void operator () (edm::Event& evt, vlq::MetCollection& MET) ;
 
@@ -22,13 +23,6 @@ private:
 	edm::InputTag l_metFulluncorPhi 	        ;
 	edm::InputTag l_metFulluncorPt 		        ;
 	edm::InputTag l_metFulluncorSumEt         	;
-	edm::InputTag l_metNoHFPhi 			;
-	edm::InputTag l_metNoHFPt 			;
-	edm::InputTag l_metNoHFPx 			;
-	edm::InputTag l_metNoHFPy 			;
-	edm::InputTag l_metNoHFuncorPhi 	;
-	edm::InputTag l_metNoHFuncorPt 		;
-	edm::InputTag l_metNoHFuncorSumEt 	;
  
 	double METPtMin_ ;
 	double METPtMax_ ;
